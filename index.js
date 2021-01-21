e78c40aee160d5737ee138f6af352d667ad0c339
vaishnavi disale <disalevaishnavi@gmail.com>
2021-01-21 15:56:08 +0530

    kbc-2

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const request = require("request");
 const { urlencoded } = require("body-parser");
 const axios = require('axios');
 //var cors = require('cors')
-global.q=[];
 const app = express();
+var q;
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(express.static(__dirname));
 //app.use(cors())
@@ -15,12 +15,15 @@ app.use(express.static(__dirname));
 app.get("/", function(req, res) {
     res.render("start");
 });
+app.get("/end", function(req, res) {
+    res.render("end");
+});
 
 //function to acsses data from request 
 //function apiCall(callback) {
- // request("https://quizzrapi.herokuapp.com/random", function(error, response, body) {
- //     var result = JSON.parse(body);
-   //   return callback(null, result);
+// request("https://quizzrapi.herokuapp.com/random", function(error, response, body) {
+//     var result = JSON.parse(body);
+//   return callback(null, result);
 //  });
 //}
 
@@ -28,44 +31,55 @@ app.get("/", function(req, res) {
 
 
 app.get("/KBC", function(req, res) {
-	//apiCall((err, body) => {
-	//res.render("kb",{que:body.question,a:body.a,b:body.b,c:body.c,d:body.d,ans:body.answer});
-  //});
-   var id = req.query.id;
-    var ans= req.query.ans;
+    //apiCall((err, body) => {
+    //res.render("kb",{que:body.question,a:body.a,b:body.b,c:body.c,d:body.d,ans:body.answer});
+    //});
+    
+    var id = req.query.id;
+    var ans = req.query.ans;
     console.log(req.query);
     axios.get('https://quizzrapi.herokuapp.com/random')
-    .then(function(response){
-       var newQuestion = response.data;
-       if(!parseInt(id))
-       {
-           console.log(id);
-        res.render("kb",{que:newQuestion.question,a:newQuestion.a,b:newQuestion.b,c:newQuestion.c,d:newQuestion.d,ans:newQuestion.answer});
-       }
-       else
-       {
-           if(newQuestion.answer==ans)
-           {
-               console.log(ans,id);
-               res.render("kb",{que:newQuestion.question,a:newQuestion.a,b:newQuestion.b,c:newQuestion.c,d:newQuestion.d,ans:newQuestion.answer});
-           }
-       }
-    })
-    
+        .then(function(response) {
+            var newQuestion = response.data;
+            if (!parseInt(id)) {
+                console.log(id);
+                global.q=newQuestion.answer;
+                console.log(q)
+                res.render("kb", { que: newQuestion.question, a: newQuestion.a, b: newQuestion.b, c: newQuestion.c, d: newQuestion.d, ans: newQuestion.answer });
+            } 
+            else {
+              console.log(q);
+                if (global.q== ans) {
+                    console.log(ans, id);
+                    global.q=newQuestion.answer;
+                    res.render("kb", { que: newQuestion.question, a: newQuestion.a, b: newQuestion.b, c: newQuestion.c, d: newQuestion.d, ans: newQuestion.answer });
+                }
+                else
+                {
+                  res.redirect("/end");
+                }
+            }
+        })
+
 });
 
 
 
-app.post("/",function(req,res){
-	//redirct from root page
-	res.redirect("/KBC?id=0");
+app.post("/", function(req, res) {
+    //redirct from root page
+    res.redirect("/KBC?id=0");
 });
 
-app.post("/verify",(req,res) => {
-    var it=req.body;
+app.post("/end", function(req, res) {
+    //redirct from root page
+    res.redirect("/");
+});
+
+app.post("/verify", (req, res) => {
+    var it = req.body;
     console.log(it);
     res.redirect('/KBC?id=1&ans=' + it.button);
-    
+
 })
 
 app.listen(5000, function() {
