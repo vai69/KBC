33e48b7c84e857bd78d9736c585d07c1584555a1
vaishnavi disale <disalevaishnavi@gmail.com>
2021-01-25 18:37:39 +0530

    KBC new

--- ajax/kbc.js
+++ ajax/kbc.js
@@ -1,1 +1,15 @@
-console.log("helllllllllllllllllllllllllllllllllllllllllll")
+var remainingTime = 30;
+var elem = document.getElementsByClassName('timer');
+console.log(elem);
+var timer = setInterval(countdown, 1000); //set the countdown to every second
+  function countdown() {
+      if (remainingTime == -1) {
+        clearTimeout(timer);
+        alert("timeout");
+    } else {
+       elem.innerHTML = remainingTime;
+       remainingTime--; //we subtract the second each iteration
+     }
+     document.getElementsByClassName('timer').textContent=remainingTime;
+     console.log(remainingTime)
+   }
